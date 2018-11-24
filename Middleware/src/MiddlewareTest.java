9d8137074c773fa117483139e54c157cb8866fcf
Adamzis <adam.j.a@hotmail.com>
2018-11-24 16:52:39 -0500

    MiddlewareTest covers argument to constructor

--- Middleware/src/MiddlewareTest.java
+++ Middleware/src/MiddlewareTest.java
@@ -7,14 +7,16 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class MiddlewareTest {
-	
-	static String poFolder;
+
+	static final String PO_FOLDER = System.getProperty("user.home") + "/PO/";
+	static final File PO_FILE = new File(PO_FOLDER);
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
-		poFolder = System.getProperty("user.home") + "/PO/";
 	}
 
 	@AfterAll
@@ -31,13 +33,21 @@ class MiddlewareTest {
 
 	@Test
 	void testMiddleware() {
-		System.out.println(poFolder);
+
+		try {
+			Middleware b2c = new Middleware(PO_FILE);
+			assertTrue(true);
+		} catch (Exception e) {
+			fail("Error thrown " + e.getMessage());
+		}
 	}
 
-	@Test
-	void testMiddlewareException() {
+	@ParameterizedTest
+	@ValueSource(strings = { "bad directory", "/eecs/adamzis/test" })
+	void testMiddlewareException(String nonsense) {
 		try {
-			File dummyfile = new File("bad directory");
+			File dummyfile = new File(nonsense);
+
 			Middleware testMiddle = new Middleware(dummyfile);
 		} catch (IllegalArgumentException e) {
 			assertTrue(true);
