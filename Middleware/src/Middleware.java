07a88e8b21f0a0d15a8891c7dc340cc1af4d0668
Adamzis <adam.j.a@hotmail.com>
2018-11-24 15:47:06 -0500

    Change input to be dynamic

--- Middleware/src/Middleware.java
+++ Middleware/src/Middleware.java
@@ -9,16 +9,20 @@ import model.*;
 
 public class Middleware {
 
-	private static final String PO_PATH = System.getProperty("user.home") + "/PO/";
-	private static final String IN_PO = PO_PATH + "inPO/";
-	private static final String OUT_PO = PO_PATH + "outPO";
+	private String poPath;
+	private String inPoPath;
+	private String outPoPath;
 
 	private JAXBContext orderContext;
 	private Marshaller orderMarshaller;
 	private Unmarshaller orderUnMarshaller;
 
-	public Middleware() {
+	public Middleware(File poDir) {
+		setOrderJAXB();
+		setPoDirs(poDir);
+	}
 
+	private void setOrderJAXB() {
 		try {
 			this.orderContext = JAXBContext.newInstance(OrderBean.class);
 			this.orderMarshaller = orderContext.createMarshaller();
@@ -29,16 +33,30 @@ public class Middleware {
 			System.err.println("Fatal error " + e.getMessage());
 			System.exit(1);
 		}
+	}
+
+	private void setPoDirs(File poDir) {
+		this.poPath = poDir.getPath();
+		this.inPoPath = poPath + "/inPO/";
+		this.outPoPath = poPath + "/outPO/";
 
 	}
 
 	public static void main(String[] args) {
-		File poDir = new File(PO_PATH);
 
-		if (!(poDir.exists() && poDir.isDirectory())) {
-			System.err.println("There is no PO file in the directory, exiting");
+		if (args.length != 1) {
+			System.err.println("Usage: java Middleware <PO Folder Path>");
 			System.exit(1);
 		}
+
+		File poDir = new File(args[0]);
+
+		if (!(poDir.exists() && poDir.isDirectory())) {
+			System.err.println("There is no PO directory, exiting");
+			System.exit(1);
+		}
+
+		Middleware service = new Middleware(poDir);
 
 	}
 
