304c21cb79b58adf43155366fd95c9f57841e368
ragheban <ragheban@ea60.eecs.yorku.ca>
2018-11-24 18:54:49 -0500

    Rae working on checkout, account, order and thankyou pages.

--- eFoods/src/ctrl/Auth.java
+++ eFoods/src/ctrl/Auth.java
@@ -14,7 +14,7 @@ import javax.servlet.http.HttpSession;
 @WebServlet("/Auth.do")
 public class Auth extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-	public static final String REDIRECT = "https://www.eecs.yorku.ca/~roumani/servers/auth/oauth.cgi?back=http://localhost:4413/eFoods/Auth.do";
+	private static final String REDIRECT = "https://www.eecs.yorku.ca/~roumani/servers/auth/oauth.cgi?back=http://localhost:4413/eFoods/Auth.do";
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
--- eFoods/src/ctrl/Order.java
+++ eFoods/src/ctrl/Order.java
@@ -1,26 +1,28 @@
 package ctrl;
 
 import java.io.IOException;
+import java.util.HashMap;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
-/**
- * Servlet implementation class Account
- */
-@WebServlet("/Account.do")
-public class Account extends HttpServlet {
+@WebServlet("/Order.do")
+public class Order extends HttpServlet {
 	private static final long serialVersionUID = 1L;
- 
+       
+  
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		this.getServletContext().getRequestDispatcher("/Account.jspx").forward(request, response);
+		// We have to set an attribute in the checkout.java called "orderName" and if that's null, then the user
+		// got the page without completing an order and we need to let them know to go awy.
+		HttpSession session = request.getSession();
+		session.setAttribute("cart", new HashMap<String, Integer>());
+		this.getServletContext().getRequestDispatcher("/Order.jspx").forward(request, response);
 	}
-
-
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
