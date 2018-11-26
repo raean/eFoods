90abeb21ac9579d17fd2f1fadee9caa9fa419dcd
Adamzis <adam.j.a@hotmail.com>
2018-11-26 17:48:44 -0500

    Listeners record time user takes.
    -Stored in two lists created when the server starts.

--- eFoods/src/listeners/Init.java
+++ eFoods/src/listeners/Init.java
@@ -1,5 +1,9 @@
 package listeners;
 
+import java.util.LinkedList;
+import java.util.List;
+
+import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
@@ -19,6 +23,13 @@ public class Init implements ServletContextListener {
 
 	public void contextInitialized(ServletContextEvent sce) {
 		Engine.getInstance();
+		ServletContext context = sce.getServletContext();
+
+		List<Integer> timeBetweenAdd = new LinkedList<>();
+		List<Integer> timeBetweenCheckout = new LinkedList<>();
+
+		context.setAttribute("timeBetweenAdd", timeBetweenAdd);
+		context.setAttribute("timeBetweenCheckout", timeBetweenCheckout);
 	}
 
 }
