34191cfcbd897ff0f852e68d23191aac093a2362
Adamzis <adam.j.a@hotmail.com>
2018-11-26 13:51:25 -0500

    Major changes to how previousOrders handled
    -previousOrders and lastOrder are now calculated upon logging in, and on
    checkout.
    -This allows previousOrders and lastOrder to be session variables.
    -This reduces engine calls and simplifies controllers that require
    access to these information
    -Both are recalculated upon a checkout.

--- eFoods/src/ctrl/Checkout.java
+++ eFoods/src/ctrl/Checkout.java
@@ -2,6 +2,7 @@ package ctrl;
 
 import java.io.IOException;
 import java.util.Map;
+import java.util.TreeMap;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,6 +10,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
+import javax.xml.bind.JAXBException;
 
 import model.CustomerBean;
 import model.Engine;
@@ -18,35 +20,52 @@ import model.OrderBean;
 @WebServlet("/Checkout.do")
 public class Checkout extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-    
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		HttpSession session = request.getSession();
 		Engine engine = Engine.getInstance();
 		request.setAttribute("cart", session.getAttribute("cart"));
 		if ((boolean) session.getAttribute("authenticated")) {
 			Map<String, Integer> cart = (Map<String, Integer>) session.getAttribute("cart");
 			CustomerBean customer = (CustomerBean) session.getAttribute("customer");
-			try {	
+			try {
 				// Let's get the cart from the session's information:
 				Map<ItemBean, Integer> viewableCart = engine.makeViewableCart(cart);
 				request.setAttribute("viewableCart", viewableCart);
-				if (request.getParameter("checkoutButton") != null && !((Map<String, Integer>) session.getAttribute("cart")).isEmpty()) {
+				if (request.getParameter("checkoutButton") != null
+						&& !((Map<String, Integer>) session.getAttribute("cart")).isEmpty()) {
 					OrderBean order = engine.makeOrder(viewableCart, customer);
 					engine.checkOut(order);
+
+					try {
+						Map<String, OrderBean> previousOrders = engine.getCustomerOrders(customer);
+
+						if (!previousOrders.isEmpty()) {
+							TreeMap<String, OrderBean> ordersTree = (TreeMap<String, OrderBean>) previousOrders;
+							OrderBean lastOrder = ordersTree.lastEntry().getValue();
+							session.setAttribute("lastOrder", lastOrder);
+						}
+
+						session.setAttribute("previousOrders", previousOrders);
+
+					} catch (JAXBException e) {
+						response.getWriter().write("Fatal error " + e.getMessage());
+					}
 				}
 			} catch (Exception e) {
 				e.printStackTrace();
 			}
-			
+
 			this.getServletContext().getRequestDispatcher("/Checkout.jspx").forward(request, response);
 		} else {
 			response.sendRedirect("Auth.do");
 		}
-		
+
 	}
 
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		doGet(request, response);
 	}
 
