9394d4c00826870bc89a45f5e1c4bdd5c4a695a2
Adamzis <adam.j.a@hotmail.com>
2018-11-27 00:07:04 -0500

    Move session inits to init.java, remove two listeners.

--- eFoods/src/listeners/Init.java
+++ eFoods/src/listeners/Init.java
@@ -1,25 +1,27 @@
 package listeners;
 
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 import javax.servlet.annotation.WebListener;
+import javax.servlet.http.HttpSessionAttributeListener;
+import javax.servlet.http.HttpSessionEvent;
+import javax.servlet.http.HttpSessionListener;
 
 import model.Engine;
 
 /**
- * Application Lifecycle Listener implementation class Init
+ * Initializes attributes upon context and session initialization.
  *
  */
 @WebListener
-public class Init implements ServletContextListener {
+public class Init implements ServletContextListener, HttpSessionListener, HttpSessionAttributeListener {
 
-	public void contextDestroyed(ServletContextEvent sce) {
-		// TODO Auto-generated method stub
-	}
 
 	public void contextInitialized(ServletContextEvent sce) {
 		Engine.getInstance();
@@ -30,6 +32,12 @@ public class Init implements ServletContextListener {
 
 		context.setAttribute("timeBetweenAdd", timeBetweenAdd);
 		context.setAttribute("timeBetweenCheckout", timeBetweenCheckout);
+	}
+
+	public void sessionCreated(HttpSessionEvent se) {
+		se.getSession().setAttribute("authenticated", false);
+		Map<String, Integer> cart = new HashMap<>();
+		se.getSession().setAttribute("cart", cart);
 	}
 
 }
