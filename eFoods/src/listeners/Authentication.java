0e665919a799ce7a42ab705262b3215dcfb1367c
Adamzis <adam.j.a@hotmail.com>
2018-11-26 23:19:51 -0500

    Remove unused methods and picture
    -Remove featured item picture when logged in on dash.jspx
    -Remove unused methods and add comment

--- eFoods/src/listeners/Authentication.java
+++ eFoods/src/listeners/Authentication.java
@@ -1,32 +1,16 @@
 package listeners;
 
-import java.util.Enumeration;
-
 import javax.servlet.annotation.WebListener;
 import javax.servlet.http.HttpSessionAttributeListener;
-import javax.servlet.http.HttpSessionBindingEvent;
 import javax.servlet.http.HttpSessionEvent;
 import javax.servlet.http.HttpSessionListener;
 
 @WebListener
 public class Authentication implements HttpSessionListener, HttpSessionAttributeListener {
 
+	// When the session is created, the user is not authenticated.
 	public void sessionCreated(HttpSessionEvent se) {
 		se.getSession().setAttribute("authenticated", false);
-	}
-
-	public void sessionDestroyed(HttpSessionEvent se) {
-	}
-
-	public void attributeAdded(HttpSessionBindingEvent se) {
-	}
-
-	public void attributeRemoved(HttpSessionBindingEvent se) {
-
-	}
-
-	public void attributeReplaced(HttpSessionBindingEvent se) {
-
 	}
 
 }
